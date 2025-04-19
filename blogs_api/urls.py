d95bf3ba1e08e77fc0628f5b62e80e99b3de2975
Light <bluelight142@gmail.com>
2025-04-20 04:22:50 +1200

    added crud and like functions for blogs

--- blogs_api/urls.py
+++ blogs_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import LoginAPIView, LogoutAPIView, RegisterAPIView
+from .views import BlogLikeAPIView, BlogListCreateView, BlogRetrieveUpdateDestroyView, LoginAPIView, LogoutAPIView, RegisterAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
     path("login/", LoginAPIView.as_view(), name="login"),
     path("logout/", LogoutAPIView.as_view(), name="logout"),
+    path("blogs/", BlogListCreateView.as_view(), name="blog-list-create"),
+    path("blogs/<int:pk>/", BlogRetrieveUpdateDestroyView.as_view(), name="blog-rud"),
+    path("blogs/<int:pk>/like/", BlogLikeAPIView.as_view(), name="blog-like"),
 ]
